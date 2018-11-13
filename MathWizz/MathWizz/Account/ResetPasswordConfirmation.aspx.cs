f6062bb7987d3807ad37fb748ff9dc8e8598158f
Felipe <f.almazan547@gmail.com>
2018-11-13 10:46:50 -0600

    new project

---
+++ MathWizz/MathWizz/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace MathWizz.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ MathWizz/MathWizz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MathWizz.Global" Language="C#" %>
